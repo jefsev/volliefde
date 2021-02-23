a2a9abafb5845b9ab5128c686e6ff3ab57ea9c08
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2021-02-23 20:24:38 +0100

    frontpage partials

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -3,5 +3,7 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.header-front-page')
+    @include('partials.section-lesson-row')
+    @include('partials.section-fw-text')
   @endwhile
 @endsection
---
+++ resources/views/partials/section-lesson-row.blade.php
@@ -0,0 +1,37 @@
+<section class="c__lesson-row">
+    <div class="bg-color"></div>
+    <img src="@asset('images/circle.svg')" alt="" class="svg-circle">
+
+    <div class="w__lesson-row boxed__m">
+        <div class="text__row boxed__s">
+            <h2>Lessen vol liefde</h2>
+            <p>ipsum dolor sit amet, consetetur sadipscing elitr, sed  diam nonumy eirmod tempor 
+                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+        </div>
+        <div class="lesson__row flex-it f-row f-just-start">
+            <a href="" class="lesson__i">
+                <img src="@asset('images/pregnant-grey.jpeg')" alt="" class="lesson__thumb">
+                <span class="location">Nijmegen ochtend</span>
+                <h3>Proefles - Zwangerschapsyoga</h3>
+                <p>Probeer een proefles op zaterdag of zondag.</p>
+                <span class="btn__def">Proefles</span>
+            </a>
+
+            <a href="" class="lesson__i">
+                <img src="@asset('images/pregnant-grey.jpeg')" alt="" class="lesson__thumb">
+                <span class="location">Nijmegen ochtend</span>
+                <h3>Proefles - Zwangerschapsyoga</h3>
+                <p>Probeer een proefles op zaterdag of zondag.</p>
+                <span class="btn__def">Proefles</span>
+            </a>
+
+            <a href="" class="lesson__i">
+                <img src="@asset('images/pregnant-grey.jpeg')" alt="" class="lesson__thumb">
+                <span class="location">Nijmegen ochtend</span>
+                <h3>Proefles - Zwangerschapsyoga</h3>
+                <p>Probeer een proefles op zaterdag of zondag.</p>
+                <span class="btn__def">Proefles</span>
+            </a>
+        </div>
+    </div>
+</section>
