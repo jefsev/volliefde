2afe0a1091256b4e2e8c48ad6664654d211c0758
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2021-03-28 20:48:36 +0200

    responsive les-single

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}"><img src="@asset('images/logo-volliefde.png')" alt=""></a>
+    <a class="brand" href="{{ home_url('/') }}"><img src="@asset('images/Hart_Judith.png')" alt=""></a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
